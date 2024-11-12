a33ed4df53bdad1aee954c7d79541a2dbbd3b13e
Yash <titanmattersbot05@gmail.com>
2024-11-12 13:03:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from pyrogram.enums import ParseMode
 import sys
 from datetime import datetime
 
-from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4, CHANNEL_ID, PORT
+from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4, CHANNEL_ID, LOG_CHANNEL, PORT
 
 
 name ="""
@@ -105,6 +105,12 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
+
+
+        if LOG_CHANNEL:
+            await self.send_message(chat_id=LOG_CHANNEL, text="Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !")
+
+        
         self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/TitanXBots")
         self.LOGGER(__name__).info(f""" \n\n
         
