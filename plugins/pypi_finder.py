2530ee64b7d6b8524281cff9c8cde045d7bb6aa0
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-20 20:53:25 +0530

    Create pypi_finder.py

---
+++ plugins/pypi_finder.py
@@ -0,0 +1,25 @@
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+def version(package_name):
+    try:
+        url = f"https://pypi.org/pypi/{package_name}/json"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            latest_version = data["info"]["version"]
+            return package_name, latest_version
+    except Exception as e:
+            print(e)
+
+@Client.on_message(filters.command("pypi") & filters.private)
+async def pypi_package(client:Client, message:Message):
+    msg = message.text.split()[1::]
+    msge = "".join(msg)
+    if not msge:
+        await message.reply_text("Incorrect the command usage -> eg:<code>/pypi SoraDBlite</code>")
+        return
+    pckage_name, latest_version = version(msge)
+    await message.reply_text(f"{pckage_name} version: {latest_version}")
