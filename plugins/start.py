ce23bcb28f6b0f1f03af425f1efff3be398cf69e
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-11-22 09:12:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,14 +1,13 @@
 import os
-import asyncio, humanize
-from pyrogram import Client, filters, __version__
+import asyncio , humanize
+from pyrogram import Client , filters , __version__
 from pyrogram.enums import ParseMode
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
+from pyrogram.types import Message , InlineKeyboardMarkup , InlineKeyboardButton , CallbackQuery
+from pyrogram.errors import FloodWait , UserIsBlocked , InputUserDeactivated
 from bot import Bot
 from config import *
-from helper_func import subscribed, encode, decode, get_messages
-from database.database import add_user, del_user, full_userbase, present_user
-
+from helper_func import subscribed , encode , decode , get_messages
+from database.database import add_user , del_user , full_userbase , present_user
 
 titanxofficials = FILE_AUTO_DELETE
 titandeveloper = titanxofficials
@@ -16,7 +15,7 @@ file_auto_delete = humanize.naturaldelta(titandeveloper)
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
-async def start_command(client: Client, message: Message):
+async def start_command(client: Client , message: Message):
     id = message.from_user.id
     if not await present_user(id):
         try:
@@ -24,9 +23,9 @@ async def start_command(client: Client, message: Message):
         except:
             pass
     text = message.text
-    if len(text)>7:
+    if len(text) > 7:
         try:
-            base64_string = text.split(" ", 1)[1]
+            base64_string = text.split(" " , 1)[1]
         except:
             return
         string = await decode(base64_string)
@@ -38,7 +37,7 @@ async def start_command(client: Client, message: Message):
             except:
                 return
             if start <= end:
-                ids = range(start,end+1)
+                ids = range(start , end + 1)
             else:
                 ids = []
                 i = start
@@ -54,18 +53,19 @@ async def start_command(client: Client, message: Message):
                 return
         temp_msg = await message.reply("Wait Bro...")
         try:
-            messages = await get_messages(client, ids)
+            messages = await get_messages(client , ids)
         except:
             await message.reply_text("Something went wrong..!")
             return
         await temp_msg.delete()
 
-        titanx_msgs = [] # List to keep track of sent message
+        titanx_msgs = []  # List to keep track of sent message
 
         for msg in messages:
 
             if bool(CUSTOM_CAPTION) & bool(msg.document):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+                caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html ,
+                                                filename=msg.document.file_name)
             else:
                 caption = "" if not msg.caption else msg.caption.html
 
@@ -75,86 +75,90 @@ async def start_command(client: Client, message: Message):
                 reply_markup = None
 
             try:
-                titanx_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                titanx_msg = await msg.copy(chat_id=message.from_user.id , caption=caption , parse_mode=ParseMode.HTML ,
+                                            reply_markup=reply_markup , protect_content=PROTECT_CONTENT)
                 # await asyncio.sleep(0.5)
                 titanx_msgs.append(titanx_msg)
-                
+
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                titanx_msg = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                titanx_msg = await msg.copy(chat_id=message.from_user.id , caption=caption , parse_mode=ParseMode.HTML ,
+                                            reply_markup=reply_markup , protect_content=PROTECT_CONTENT)
                 titanx_msgs.append(titanx_msg)
-                
+
             except:
                 pass
-                
-        k = await client.send_message(chat_id = message.from_user.id, text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Video / File Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Video / File To Somewhere Else And Start Downloading There.")
+
+        k = await client.send_message(chat_id=message.from_user.id ,
+                                      text=f"<b>❗️ <u>IMPORTANT</u> ❗️</b>\n\nThis Video / File Will Be Deleted In {file_auto_delete} (Due To Copyright Issues).\n\n📌 Please Forward This Video / File To Somewhere Else And Start Downloading There.")
 
         # Schedule the file deletion
-        asyncio.create_task(delete_files(titanx_msgs, client, k))
+        asyncio.create_task(delete_files(titanx_msgs , client , k))
 
         # for titanx_msg in titanx_msgs:
-            # try:
-                # await titanx_msg.delete()
-                # await k.edit_text("Your Video / File Is Successfully Deleted✅")
-            # except:
-                # pass: 
-             
+        # try:
+        # await titanx_msg.delete()
+        # await k.edit_text("Your Video / File Is Successfully Deleted✅")
+        # except:
+        # pass:
+
         return
     else:
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("🧠 ʜᴇʟᴘ", callback_data = "help"),
-                    InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ", callback_data = "about")
-                ],
+                    InlineKeyboardButton("🧠 ʜᴇʟᴘ" , callback_data="help") ,
+                    InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ" , callback_data="about")
+                ] ,
                 [
-                    InlineKeyboardButton("🔐 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", url = "https://github.com/Jisshubot/Jisshu-filter-bot")
+                    InlineKeyboardButton("💻 ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ" , user_id=5356695781),
+                    InlineKeyboardButton("🔐 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ" , url="https://github.com/Jisshubot/Jisshu-filter-bot")
                 ]
             ]
         )
         await message.reply_photo(
-            photo= START_PIC,
-            caption= START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            
+            photo=START_PIC ,
+            caption=START_MSG.format(
+                first=message.from_user.first_name ,
+                last=message.from_user.last_name ,
+                username=None if not message.from_user.username else '@' + message.from_user.username ,
+                mention=message.from_user.mention ,
+                id=message.from_user.id
+            ) ,
+            reply_markup=reply_markup ,
+
         )
         return
-    
 
-#=====================================================================================##
+
+# =====================================================================================##
 
 WAIT_MSG = "<b>Working....</b>"
 
 REPLY_ERROR = "<code>Use this command as a reply to any telegram message without any spaces.</code>"
 
-#=====================================================================================##
 
-    
-    
+# =====================================================================================##
+
+
 @Bot.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
+async def not_joined(client: Client , message: Message):
     buttons = [
         [
-            InlineKeyboardButton(text="✵ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✵", url=client.invitelink2),
-        ],
+            InlineKeyboardButton(text="✵ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ" , url=client.invitelink) ,
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✵" , url=client.invitelink2) ,
+        ] ,
         [
-            InlineKeyboardButton(text="✵ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink3),
-            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✵", url=client.invitelink4),
+            InlineKeyboardButton(text="✵ ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ" , url=client.invitelink3) ,
+            InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ ✵" , url=client.invitelink4) ,
         ]
     ]
     try:
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = '☢ ɴᴏᴡ ᴄʟɪᴄᴋ ʜᴇʀᴇ •',
-                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                    text='☢ ɴᴏᴡ ᴄʟɪᴄᴋ ʜᴇʀᴇ •' ,
+                    url=f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
         )
@@ -162,25 +166,27 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply_photo(
-    photo=FORCE_PIC, 
-    caption=FORCE_MSG.format(
-        first=message.from_user.first_name,
-        last=message.from_user.last_name,
-        username=None if not message.from_user.username else '@' + message.from_user.username,
-        mention=message.from_user.mention,
-        id=message.from_user.id
-    ),
-    reply_markup=InlineKeyboardMarkup(buttons)
-)
+        photo=FORCE_PIC ,
+        caption=FORCE_MSG.format(
+            first=message.from_user.first_name ,
+            last=message.from_user.last_name ,
+            username=None if not message.from_user.username else '@' + message.from_user.username ,
+            mention=message.from_user.mention ,
+            id=message.from_user.id
+        ) ,
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
-async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
+async def get_users(client: Bot , message: Message):
+    msg = await client.send_message(chat_id=message.chat.id , text=WAIT_MSG)
     users = await full_userbase()
     await msg.edit(f"{len(users)} users are using this bot")
 
+
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
-async def send_text(client: Bot, message: Message):
+async def send_text(client: Bot , message: Message):
     if message.reply_to_message:
         query = await full_userbase()
         broadcast_msg = message.reply_to_message
@@ -189,7 +195,7 @@ async def send_text(client: Bot, message: Message):
         blocked = 0
         deleted = 0
         unsuccessful = 0
-        
+
         pls_wait = await message.reply("<i>ʙʀᴏᴀᴅᴄᴀꜱᴛ ᴘʀᴏᴄᴇꜱꜱɪɴɢ....</i>")
         for chat_id in query:
             try:
@@ -209,7 +215,7 @@ async def send_text(client: Bot, message: Message):
                 unsuccessful += 1
                 pass
             total += 1
-        
+
         status = f"""<b><u>ʙʀᴏᴀᴅᴄᴀꜱᴛ...</u>
 
 Total Users: <code>{total}</code>
@@ -217,7 +223,7 @@ Successful: <code>{successful}</code>
 Blocked Users: <code>{blocked}</code>
 Deleted Accounts: <code>{deleted}</code>
 Unsuccessful: <code>{unsuccessful}</code></b>"""
-        
+
         return await pls_wait.edit(status)
 
     else:
@@ -226,14 +232,12 @@ Unsuccessful: <code>{unsuccessful}</code></b>"""
         await msg.delete()
 
 
-
-
 # Function to handle file deletion
-async def delete_files(messages, client, k):
+async def delete_files(messages , client , k):
     await asyncio.sleep(FILE_AUTO_DELETE)  # Wait for the duration specified in config.py
     for msg in messages:
         try:
-            await client.delete_messages(chat_id=msg.chat.id, message_ids=[msg.id])
+            await client.delete_messages(chat_id=msg.chat.id , message_ids=[msg.id])
         except Exception as e:
             print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
     # await client.send_message(messages[0].chat.id, "Your Video / File Is Successfully Deleted ✅")
