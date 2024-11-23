cdeb2afffb29557466217a5c489a6265648d3ba5
Yash <titanmattersbot05@gmail.com>
2024-11-23 13:36:23 +0530

    Add files via upload

---
+++ plugins/cbb (1).py
@@ -0,0 +1,60 @@
+#TitanXBots
+
+from pyrogram import Client
+from bot import Bot
+from config import *
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from database.database import add_user, del_user, full_userbase, present_user
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "help":
+        await query.message.edit_text(
+            text=HELP_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data = "start"),
+                    InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=ABOUT_TXT.format(first=query.from_user.first_name),
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚓ ʜᴏᴍᴇ", callback_data = "start"),
+                    InlineKeyboardButton("⚡ ᴄʟᴏꜱᴇ", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "start":
+        await query.message.edit_text(
+            text=START_MSG.format(first=query.from_user.first_name),
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🧠 ʜᴇʟᴘ" , callback_data="help") ,
+                        InlineKeyboardButton("🔰 ᴀʙᴏᴜᴛ" , callback_data="about")
+                    ] ,
+                    [
+                        InlineKeyboardButton("💻 ᴄᴏɴᴛᴀᴄᴛ ᴏᴡɴᴇʀ" , user_id=5356695781) ,
+                        InlineKeyboardButton("🔐 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ" , url="https://github.com/Jisshubot/Jisshu-filter-bot")
+                    ]
+                ]
+            )
+        )            
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
